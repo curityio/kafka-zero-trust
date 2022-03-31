bc795937c267061ff914e9e35ba6f138f4845d73
gary.archer <gary.archer@WORKMAC.local>
2022-03-31 12:30:48 +0200

    Updates

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,3 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 # Run the Docker Compose network
 #
 docker compose -p kafka up
-if [ $? -ne 0 ]; then
-  echo "Problem encountered building the Sales API Docker image"
-  exit 1
-fi
